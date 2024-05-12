22c84bd092b8291c25d3ca473eb94c145e336c0f
Aliardaozdemir <arda.ozdemir.hacettepe@gmail.com>
2024-05-12 17:06:55 +0300

    RC-0: Add profile page

---
+++ app/profile/[id]/page.tsx
@@ -0,0 +1,59 @@
+'use client'
+
+import Navbar from '@/components/Navbar'
+import { NavLink, User } from '@/types'
+import React, { useEffect, useState } from 'react'
+
+interface ProfilePageProps {
+  params: { id: string }
+}
+
+export default function ProfilePage({ params }: ProfilePageProps) {
+  const { id } = params
+  const [user, setUser] = useState<User>()
+  const [navBarLinks, setNavbarLinks] = useState<NavLink[]>([])
+  useEffect(() => {
+    const links = JSON.parse(localStorage.getItem('navBarLinks') || '[]')
+    const userFromStorage = JSON.parse(
+      localStorage.getItem('user') || 'null'
+    ) as User
+    setUser(userFromStorage)
+    setNavbarLinks(links)
+  }, [])
+
+  return (
+    <div className="flex flex-col items-center">
+      <Navbar links={navBarLinks} username={user?.email || ''} />
+      <div className="my-8 space-y-6">
+        <div className="flex space-x-4">
+          <div className="border rounded-lg shadow-lg p-6 max-w-sm">
+            <p className="font-bold text-black mb-2">First Name:</p>
+            <div className="text-blue-400 text-2xl font-bold mb-4">
+              {user?.first_name}
+            </div>
+          </div>
+          <div className="border rounded-lg shadow-lg p-6 max-w-sm">
+            <p className="font-bold text-black mb-2">Last Name:</p>
+            <div className="text-blue-400 text-2xl font-bold mb-4">
+              {user?.last_name}
+            </div>
+          </div>
+        </div>
+        <div className="border rounded-lg shadow-lg p-6 max-w-sm">
+          <p className="font-bold text-black mb-2">Email:</p>
+          <div className="text-blue-400 text-2xl font-bold mb-4">
+            {user?.email}
+          </div>
+        </div>
+        <div className="border rounded-lg shadow-lg p-6 max-w-sm">
+          <p className="font-bold text-black mb-2">Created Forms:</p>
+          <p className="text-blue-400 text-2xl font-bold mb-4">something</p>
+        </div>
+        <div className="border rounded-lg shadow-lg p-6 max-w-sm">
+          <p className="font-bold text-black mb-2">Attended Forms:</p>
+          <p className="text-blue-400 text-2xl font-bold mb-4">something</p>
+        </div>
+      </div>
+    </div>
+  )
+}
