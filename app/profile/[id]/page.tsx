197f14fe88886948b83284496782f03c5a8c5a7c
blurlander <efezdn@hotmail.com>
2024-05-15 21:30:48 +0300

    RC-0: Fix styling in profile

--- app/profile/[id]/page.tsx
+++ app/profile/[id]/page.tsx
@@ -1,7 +1,6 @@
 'use client'
 
-import Navbar from '@/components/Navbar'
-import { NavLink, User } from '@/types'
+import { User } from '@/types'
 import React, { useEffect, useState } from 'react'
 
 interface ProfilePageProps {
@@ -11,45 +10,41 @@ interface ProfilePageProps {
 export default function ProfilePage({ params }: ProfilePageProps) {
   const { id } = params
   const [user, setUser] = useState<User>()
-  const [navBarLinks, setNavbarLinks] = useState<NavLink[]>([])
   useEffect(() => {
-    const links = JSON.parse(localStorage.getItem('navBarLinks') || '[]')
     const userFromStorage = JSON.parse(
       localStorage.getItem('user') || 'null'
     ) as User
     setUser(userFromStorage)
-    setNavbarLinks(links)
   }, [])
 
   return (
-    <div className="flex flex-col items-center">
-      <Navbar links={navBarLinks} username={user?.email || ''} />
-      <div className="my-8 space-y-6">
+    <div className="flex flex-col items-center ">
+      <div className="my-8 space-y-6 min-w-[50rem]">
         <div className="flex space-x-4">
-          <div className="border rounded-lg shadow-lg p-6 max-w-sm">
+          <div className="border rounded-lg shadow-lg p-6 w-[25rem]">
             <p className="font-bold text-black mb-2">First Name:</p>
             <div className="text-blue-400 text-2xl font-bold mb-4">
               {user?.first_name}
             </div>
           </div>
-          <div className="border rounded-lg shadow-lg p-6 max-w-sm">
+          <div className="border rounded-lg shadow-lg p-6 w-[25rem]">
             <p className="font-bold text-black mb-2">Last Name:</p>
             <div className="text-blue-400 text-2xl font-bold mb-4">
               {user?.last_name}
             </div>
           </div>
         </div>
-        <div className="border rounded-lg shadow-lg p-6 max-w-sm">
+        <div className="border rounded-lg shadow-lg p-6">
           <p className="font-bold text-black mb-2">Email:</p>
           <div className="text-blue-400 text-2xl font-bold mb-4">
             {user?.email}
           </div>
         </div>
-        <div className="border rounded-lg shadow-lg p-6 max-w-sm">
+        <div className="border rounded-lg shadow-lg p-6 ">
           <p className="font-bold text-black mb-2">Created Forms:</p>
           <p className="text-blue-400 text-2xl font-bold mb-4">something</p>
         </div>
-        <div className="border rounded-lg shadow-lg p-6 max-w-sm">
+        <div className="border rounded-lg shadow-lg p-6">
           <p className="font-bold text-black mb-2">Attended Forms:</p>
           <p className="text-blue-400 text-2xl font-bold mb-4">something</p>
         </div>
